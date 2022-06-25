1b527cf4d08e84531484a6028fa726ffd781c44a
Gerard <gerard.ros@nmi-agro.nl>
2022-06-25 23:03:10 +0200

    reset visualise pcs maps

--- scripts/visualize psc maps.R
+++ scripts/visualize psc maps.R
@@ -1,76 +1,24 @@
 ####visualization of raster images
 
 #load packages
-require(terra); require(ggplot2); require(ggspatial) ; require(data.table)
+library(terra) 
+library(ggplot2)
+library(ggspatial)
 
-  # clear environment
-  rm(list= ls())
+#set wd
+setwd(paste0("C:/Users/", Sys.info()[["user"]], "/SPRINGG/Gerard Ros - NMI-PROJ/JustPmaps/"))
 
-  # set wd if no link to local data is required
-  # setwd(paste0("C:/Users/", Sys.info()[["user"]], "/SPRINGG/Gerard Ros - NMI-PROJ/JustPmaps/"))
+#set ggplot theme
+theme_set(theme_bw())
 
-  #set ggplot theme
-  theme_set(theme_bw())
+#homosline projection
+homosoline <- "+proj=igh +lat_0=0 +lon_0=0 +datum=WGS84 +units=m +no_defs" #homosoline projection
 
-  #homosline projection
-  homosoline <- "+proj=igh +lat_0=0 +lon_0=0 +datum=WGS84 +units=m +no_defs" 
+#load rasters
+alox <- rast("03 output/alox.tiff")
+feox <- rast("03 output/feox.tiff")
+both <- rast("03 output/fe_alox.tiff")
 
-  # load the poxptot model
-  poxptotlm <- readRDS('data/poxptot_lm.RDS')
-    
-  # load rasters with predicted Al and Fe oxides (made by Maarten)
-  alox <- rast("data/alox.tiff")
-  feox <- rast("data/feox.tiff")
-  both <- rast("data/fe_alox.tiff")
-  
-  # load crop P uptake in arable land (from IMAGE) and convert to boolean (there is arable crop or not)
-  crop <- rast('data/crop_puptake.asc')
-  crop[crop==0] <- NA
-  crop <- terra::resample(crop,alox,method ='near')
-  crop[!is.na(crop)] <- 1
-  
-  # update alox and feox
-  alox2 <- alox * crop
-  feox2 <- feox * crop
-  alfeox2 <- both * crop
-  
-  # read in relevant data from IMAGE
-  dens <- rast('D:/DATA/07 image/bulkdensity_cr.asc')
-  dens <- terra::resample(dens,alox,method ='bilinear')
-  ptot <- rast('D:/DATA/07 image/Ptot.asc')
-  ptot <- terra::resample(ptot,alox,method ='bilinear')
-  
-  # stack them
-  spr <- c(alox2,feox2,alfeox2,ptot,dens)
-  
-  # estimate just P levels
-  
-    # set to data.table
-    dt <- as.data.table(as.data.frame(spr, xy=TRUE, na.rm=F))
-  
-    # estimate pox-ptot fraction given fe_alox in mmol/kg, function trained over full PSI range in 'pox-pot-regression.R'
-    dt[, pox_ptot := 0.16352 + 0.24635 * log10(fe_alox)]
-
-    # estiamate current PSI (as fraction (-)
-    dt[,psi_current := (Ptot / 31) * pox_ptot / fe_alox]
-      
-    # set PSI target to 0.1
-    dt[,psi_target := 0.1]
-    
-    # depth of the soil layer
-    dt[, depth := 0.3]
-    
-    # duration to reach the target (years)
-    dt[,duration := 35]
-    
-    # estimate additional P input given PSI target = 0.1
-    dt[, preq1 := bulkdensity_cr * 1000 * depth * pmax(0,0.1 - psi_current) * fe_alox * (1/pox_ptot) * 31 * 0.01 / duration]
-    
-    # estimate additional P input given PSI target = 0.1
-    dt[, preq2 := bulkdensity_cr * 1000 * depth * pmax(0,0.15 - psi_current) * fe_alox * (1/pox_ptot) * 31 * 0.01 / duration]
-    
-    
-  
 #visualize
 visualize <- function(raster, name, breaks){
   #convert rast to homosline
